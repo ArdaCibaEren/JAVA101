873a40398397725aa0ee30ab07a758c1fe3700e8
ArdaCibaEren <109301260+ArdaCibaEren@users.noreply.github.com>
2022-09-01 14:09:18 +0300

    Add files via upload

---
+++ Harmonik-Seri/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner inp = new Scanner(System.in);
+        System.out.println("n sayısını giriniz.");
+        int n = inp.nextInt();
+        double result = 0.0;
+        for (int i = 1; i <= n; i++) {
+            result += (1.0 / i);
+        }
+        System.out.println(result);
+    }
+}
+//www.patika.dev
