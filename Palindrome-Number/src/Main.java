52adb9ca02c9c3137ee110be0fa2b19738c53324
ArdaCibaEren <109301260+ArdaCibaEren@users.noreply.github.com>
2022-09-05 16:09:10 +0300

    Add files via upload

---
+++ Palindrome-Number/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static boolean isPalindrom(int number) {
+        int temp = number, reverseNumber = 0, lastNumber;
+        while (temp != 0) {
+            lastNumber = temp % 10;
+            reverseNumber = (reverseNumber * 10) + lastNumber;
+            temp /= 10;
+        }
+        if (number == reverseNumber) {
+            System.out.println("This is a palindrome number.");
+            return true;
+        } else {
+            System.out.println("This is not a palindrome number.");
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Please enter a number : ");
+        int i = inp.nextInt();
+        System.out.println(isPalindrom(i));
+    }
+}
