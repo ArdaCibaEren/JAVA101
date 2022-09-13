c74d3a13dc667ec7c0a0a5f5163f2298074acf68
ArdaCibaEren <109301260+ArdaCibaEren@users.noreply.github.com>
2022-09-13 15:40:57 +0300

    Delete Boks-Maci/src directory

--- Boks-Maci/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Fighter marc = new Fighter("Marc" , 15 , 100, 90, 20);
-        Fighter alex = new Fighter("Alex" , 10 , 95, 100, 20);
-        Match r = new Match(marc,alex , 90 , 100);
-        r.run();
-    }
-}
